f0461219e02d28a931c8f49e48b2300ab5522a4f
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-02-17 22:00:22 -0300

    Refactoring Reducer and created an action according to what was passed on change value of input

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import AppReducer from './AppReducer';
 
 import './index.css';
 import App from './App';
 
 const reducers = combineReducers({
-  field: () => ({ value: 'opa' })
+  field: AppReducer
 });
 
 ReactDOM.render(
